bf3b822953bd0a9ef5ef56c6ba854b7ffd0a7771
Batman <Batman@DESKTOP-EFVKIOI>
2023-03-01 20:02:36 +0700

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //вариант1 12 - Журнал группы
+
+        }
+    }
+}

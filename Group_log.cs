4e25c74912aefd0f68579b807ffd5cccfc825412
maksimilyan.lamaev.13@inbox.ru <maksimilyan.lamaev.13@inbox.ru>
2023-03-02 11:03:13 +0700

    Доработать

---
+++ Group_log.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PZ_LinQ_Lamaev
+{
+    public class Group_log
+    {
+        public string Classroom_teacher { get; set; }
+        public List<string> Items { get; set; } = new List<string>();
+        public List<string> Students { get; set; } = new List<string>();
+        
+        public Group_log(string classroom_teacher, List<string> items, List<string> students)
+        {
+            this.Classroom_teacher = classroom_teacher;
+            this.Items = items;
+            this.Students = students;
+
+        }
+
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,13 +1,37 @@
-﻿using System;
+﻿using PZ_LinQ_Lamaev;
+using System;
+using System.Collections.Generic;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
-    internal class Program
-    {
+     class Program
+     {
+        static List<Group_log> group_s;
+        static List<string> Items1 = new List<string> { "Математика", "Русский язык", "География", "Химия" };
+        static List<string> Students1 = new List<string> { "Молчанов", "Петров", "Андреев", "Лебедев" };
+        static List<Group_log> group_а;
+        static List<string> Items2 = new List<string> { "Математика", "Русский язык", "География" };
+        static List<string> Students2 = new List<string> { "Молчанов", "Петров", "Андреев", "Лебедев", "Григорий" };
+
+
+
         static void Main(string[] args)
         {
-            //вариант1 12 - Журнал группы
-
+            group_s =  new List<Group_log>()
+            {
+                new Group_log("Олег",Items1,Students1)
+            };
+            group_а = new List<Group_log>()
+            {
+                new Group_log("Антон", Items2,Students2)
+            };
+            //var kok = group_s.Select(u => u.Items.Where(u=>u == "Математика")).ToList();
+            //kok.ForEach(u =>Console.WriteLine(u)); 
+            var a = group_а.Where(u => u.Items.Count >= 3 && u.Students.Count >= 5).ToList();
+            a.ForEach(u => Console.WriteLine(u.Classroom_teacher));
         }
+           
     }
+
+    
 }
